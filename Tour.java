9b24558fc5798aa2289942cb0e0eb81ac1fbef68
abigailmoss <abby@themosses.com>
2019-04-21 09:50:02 -0400

    Create Tour.java
    Start project

---
+++ Tour.java
@@ -0,0 +1,20 @@
+
+public class Tour {
+
+	public static void main(String[] args) {
+		int[][] Cities = {
+				{0, 132, 217, 164,58},
+				{132, 0, 290, 201, 79},
+				{217, 290, 0, 113, 303},
+				{164, 201, 113, 0, 196},
+				{58, 79, 303, 196, 0}
+		};
+		int total_distance = 0;
+		int nearest_city = 0;
+		for(int i = 0; i<Cities.length; i++) {
+			for(int j = 0; j<Cities[i].length; j++) {
+				
+			}
+		}
+	}
+}
